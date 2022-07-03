4c609e898c7d846e3c8b0e5eeecb6f974bfcc2c1
Mazayw <mazayw@gmail.com>
2022-07-03 19:36:20 +0300

    feat: apply ts to sources

--- src/components/view/sources/sources.ts
+++ src/components/view/sources/sources.ts
@@ -1,12 +1,22 @@
 import './sources.css';
 
-class Sources {
-    draw(data) {
-        const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp');
+interface ISource {
+    readonly id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+}
 
-        data.forEach((item) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true);
+class Sources {
+    draw(data: ISource[]) {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement = document.querySelector('#sourceItemTemp');
+
+        data.forEach((item: ISource) => {
+            const sourceClone = <DocumentFragment>sourceItemTemp.content.cloneNode(true);
 
             sourceClone.querySelector('.source__item-name').textContent = item.name;
             sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
